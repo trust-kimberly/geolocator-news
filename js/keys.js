ea3c7c51418071a2f030e6b39ef5f732225e8b2a
trust-kimberly <kimberlynguyen.xyz@gmail.com>
2021-08-01 16:08:05 -0400

    keys

---
+++ js/keys.js
@@ -0,0 +1,9 @@
+export const mapKey = 'Cp9bOOWstOVFluXWybsJ6XxcsHhQysbU';
+
+export const weatherKey = "03f2fcada84e9d659128090e23fe669b";
+
+export const newsKey = 'aa2e771fe28b4cb0b94ff04ceec946ac';
+
+export const nytKey = 'H5bIZfEwUK6Dh7wGKMgsrMA2f9lCPAUj';
+
+
