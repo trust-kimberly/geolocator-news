f1ae2a633a37b1834f78587609a0444873bdd050
trust-kimberly <kimberlynguyen.xyz@gmail.com>
2021-08-01 23:59:23 -0400

    geolocation, api calls, and search bar functionality

--- js/app.js
+++ js/app.js
@@ -16,161 +16,188 @@ if ('geolocation' in navigator) {
 // async func for mapquest api call
 async function reverseGeolocate(url) {
     try {
-        const rawResponse = await fetch(url);
-    
+        const rawResponse = await fetch(url);    
         if (!rawResponse.ok) {
           throw new Error(rawResponse.message);
         }
-    
         if (rawResponse.status === 404) {
           throw new Error('Not found');
         }
-    
         const jsonResponse = await rawResponse.json();
-        
         // grabbing city
         let city = jsonResponse.results[0].locations[0].adminArea5;
-        
         // updating ui
         document.getElementById('city').innerHTML = city;
         document.getElementById('city-wiki').href = 'https://en.wikipedia.org/wiki/' + city;
-        
         // running getWeather api call and ui update
         getWeather(city);
         return jsonResponse, city;
-
-      } catch (err) {
+    } catch (err) {
         console.log('err', err);
-      }
+    }
 }
 
-// get weather - only works for cities in the us
+// get weather - only works for cities in the us for now
 async function getWeather(city) {
     // API URL w imported key
     const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=" + weatherKey;
-    
     try {
         // fetch the raw response
         const rawResponse = await fetch(weatherUrl);
-    
         // rawResponse.ok is true if status code is between 200 - 299
         if (!rawResponse.ok) {
           throw new Error(rawResponse);
         }
-    
         const jsonResponse = await rawResponse.json();
         console.log('jsonResponse', jsonResponse);
-    
         // grabbing temp
         let temp = JSON.parse(jsonResponse.main.temp);
-    
         // converting kelvin to fahrenheit
         let tempF = ((temp - 273.15) * (9/5) + 32).toFixed(1);
         console.log(tempF);
         console.log(city);
-    
         // updating UI
         document.getElementById('temp').innerHTML = tempF;
-    
-      } catch (err) {
+    } catch (err) {
         console.log('err', err);
-      }
-    
+    }  
 }
 
-
-
-
-// news api calls, then function to filter news by location 
+// news sources api calls
 let newsSources = [
     'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + newsKey,
     'https://cors.bridged.cc/https://www.reddit.com/top.json',
     'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=' + nytKey
-  ];
+];
   
-  function renderRows(data) {
+function renderRows(data) {
     let mainElem = document.getElementById('main');
     let articleElem = document.createElement('article');
     articleElem.className = 'article';
     articleElem.innerHTML = `
-      <section class="featuredImage">
-        <img src="${data.img}" alt="" />
-      </section>
-      <section class="articleContent">
-        <a href="${data.url}" target="_blank"><h3>${data.title}</h3></a>
-        <h6>Author: ${data.author}</h6>
-        <h6>Source: ${data.source}</h6>
-      </section>
-      <section class="impressions">
-        ${data.date}
-      </section>
-      <div class="clearfix"></div>`;
+        <section class="featuredImage">
+            <img src="${data.img}" alt="" />
+        </section>
+        <section class="articleContent">
+            <h3>${data.title}</h3>
+            <h6>Author: ${data.author}</h6>
+            <h6>Source: ${data.source}</h6>
+        </section>
+        <section class="impressions">
+            Published:<br>
+            ${data.date}
+        </section>
+        <div class="clearfix"></div>
+        `;
+    // adding event listeners and updating popup
+    articleElem.addEventListener('click', function() {
+        const popUp = document.getElementById('popUp');
+        popUp.classList.remove('hidden');
+        popUp.innerHTML = `
+        <a id="closePopUp">X</a>
+        <div class="container">
+            <h1>${data.title}</h1>
+            <p>${data.content}</p>
+            <p>Author: ${data.author}</p>
+            <p>Source: ${data.source}</p>
+            <a href="${data.url}" class="popUpAction" target="blank">Read more from source</a>
+        </div>
+        `; 
+        return popUp;
+    });
+    // popup hidden on click
+    popUp.addEventListener('click', function() {
+        popUp.classList.add('hidden');
+    })
     mainElem.appendChild(articleElem);
-  }
+    // search bar script
+    const submit = document.getElementById('submit-search');
+    submit.addEventListener('click', function() {
+        let textInput = document.getElementById('search-input').value;
+        console.log(textInput);
+        let children = mainElem.children;
+        for (let index = 0; index < children.length; index++) {
+            if (children[index].innerHTML.includes(textInput)) {
+                console.log('matches found');
+                children[index].style.display = 'block';
+            } else if (!children[index].innerHTML.includes(textInput)) {
+                children[index].style.display = 'none';
+            }
+        };
+    });
+    const reset = document.getElementById('reset-search');
+    reset.addEventListener('click', function() {
+        document.getElementById('search-input').value = '';
+        let children = mainElem.children;
+        for (let index = 0; index < children.length; index++) {
+            children[index].style.display = 'block';
+        }
+    });
+}
   
-  async function retrieveData(dataUrl) {
+async function retrieveData(dataUrl) {
     try {
-      const rawResponse = await fetch(dataUrl);
+        const rawResponse = await fetch(dataUrl);
   
-      if (!rawResponse.ok) {
-        throw new Error(rawResponse.message);
-      }
+        if (!rawResponse.ok) {
+            throw new Error(rawResponse.message);
+        }
   
-      if (rawResponse.status === 404) {
-        throw new Error('Not found');
-      }
+        if (rawResponse.status === 404) {
+            throw new Error('Not found');
+        }
   
-      const jsonResponse = await rawResponse.json();
-      console.log(jsonResponse);
-      return jsonResponse;
+        const jsonResponse = await rawResponse.json();
+        console.log(jsonResponse);
+        return jsonResponse;
     } catch (err) {
-      console.log('err', err);
+        console.log('err', err);
     }
-  }
+}
   
-  function normalizeData(data) {
+function normalizeData(data) {
     console.log('data', data);
-    function ArticleObj(title, author, url, img, source, date) {
-      this.title = title;
-      this.author = author;
-      this.url = url;
-      this.img = img;
-      this.source = source;
-      this.date = date;
+    function ArticleObj(title, author, url, img, source, date, content) {
+        this.title = title;
+        this.author = author;
+        this.url = url;
+        this.img = img;
+        this.source = source;
+        this.date = date;
+        this.content = content;
     }
     for (let i = 0; i < data.length; i++) {
-      let cleanData = [];
-      if(i === 0) { 
-        //news api
-        data[i].articles.forEach(function(result) {
-          cleanData.push(new ArticleObj(result.title, result.author, result.url, result.urlToImage, result.source.name, result.publishedAt));
-        });
-        data[i] = cleanData;
-      } else if(i === 1) { 
-        //reddit
-        data[i].data.children.forEach(function(result) {
-          cleanData.push(new ArticleObj(result.data.title, result.data.author, result.data.url, result.data.thumbnail, result.data.subreddit_name_prefixed, result.data.created));
-        });
-        data[i] = cleanData;
-      } else if(i === 2) {
-        // nyt
-        data[i].results.forEach(function(result) {
-          cleanData.push(new ArticleObj(result.title, result.byline, result.url, result.multimedia[1], result.section, result.created_date));
-        });
-        data[i] = cleanData;
-          }
+        let cleanData = [];
+        if(i === 0) { 
+            //news api
+            data[i].articles.forEach(function(result) {
+                cleanData.push(new ArticleObj(result.title, result.author, result.url, result.urlToImage, result.source.name, result.publishedAt, result.description));
+            });
+            data[i] = cleanData;
+        } else if(i === 1) { 
+            //reddit
+            data[i].data.children.forEach(function(result) {
+                cleanData.push(new ArticleObj(result.data.title, result.data.author, result.data.url, result.data.thumbnail, result.data.subreddit_name_prefixed, result.data.created, result.data.subreddit_name_prefixed));
+            });
+            data[i] = cleanData;
+        } else if(i === 2) {
+            // nyt
+            data[i].results.forEach(function(result) {
+                cleanData.push(new ArticleObj(result.title, result.byline, result.url, result.multimedia[0].url, result.section, result.created_date, result.abstract));
+            });
+            data[i] = cleanData;
+        }
     }
     return data;
-  }
+}
   
-  // loads data
-  async function init(sources) {
+// loads data
+async function init(sources) {
     // retrievesData from the various sources listed in array above
     let promises = [];
     for (let i = 0; i < sources.length; i++) {
-      promises.push(retrieveData(sources[i]));
+        promises.push(retrieveData(sources[i]));
     }
-  
     // awaits that all data has been loaded before using
     const newsData = await Promise.all(promises);
     // normalize, have data in a more predicatable fashion since all api data is different
@@ -178,12 +205,14 @@ let newsSources = [
   
     // for each set of data, we want to loop through and append to our page
     cleanData.forEach(function(sources) {
-      // looping through each individual set of data per api
-      sources.forEach(function(articles) {
-        // appending the article rows to the DOM
-        renderRows(articles);
-      });
+        // looping through each individual set of data per api
+        sources.forEach(function(articles) {
+            // appending the article rows to the DOM
+            renderRows(articles);
+        });
     });
-  }
+}
   
-  init(newsSources);
+init(newsSources);
+
+// need function that filters data based on geolocation
